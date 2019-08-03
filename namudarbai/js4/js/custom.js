44e7fd9b922c9a645a417bf8c185740a551626a7
Inga <kriukovaitei@gmail.com>
2019-08-03 21:20:10 +0300

    Pridėtas namų darbas JS4

---
+++ namudarbai/js4/js/custom.js
@@ -0,0 +1,11 @@
+function ivestasAmzius(){
+	if(isNaN(amzius)){
+	alert ("Įveskite skaičių");
+}
+	if (amzius < 18) {
+	alert ("Jūs negalite užeiti į svetainę");
+}
+	else if (amzius >=18) {
+	alert ("Jūs galite užeiti į svetainę");
+}
+}
