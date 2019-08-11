7afb054c00506fab363e597db51c1ed48f7b0ffe
Inga <kriukovaitei@gmail.com>
2019-08-11 19:58:31 +0300

    Pridėtas projektas

---
+++ 2019projektas/js/service.js
@@ -0,0 +1,13 @@
+function getGallery(){
+	return [
+		{ imageLink: "https://picsum.photos/id/1033/800/800", title: "Work1", id: 1},
+		{ imageLink: "https://picsum.photos/id/134/800/800", title: "Work2", id: 2},
+		{ imageLink: "https://picsum.photos/id/157/800/800", title: "Work3", id: 3},
+		{ imageLink: "https://picsum.photos/id/180/800/800", title: "Work4", id: 4},
+		{ imageLink: "https://picsum.photos/id/284/800/800", title: "Work5", id: 5},
+		{ imageLink: "https://picsum.photos/id/297/800/800", title: "Work6", id: 6},
+		{ imageLink: "https://picsum.photos/id/328/800/800", title: "Work7", id: 7},
+		{ imageLink: "https://picsum.photos/id/348/800/800", title: "Work8", id: 8},
+		{ imageLink: "https://picsum.photos/id/364/800/800", title: "Work9", id: 9}
+	];
+}
